184d696f420adb71da996db4e1e0d9b1d65c11a9
aditya pratap <pratapaditya84@gmail.com>
2023-09-30 12:47:39 +0530

    j

--- src/components/share/share.jsx
+++ src/components/share/share.jsx
@@ -18,7 +18,7 @@ const Share = () => {
       formData.append("image", file)
        axios.post('https://api.imgbb.com/1/upload?key=b99e1b7e44deb3985e33be22d597e53f', formData)  
        .then((res)=>{
-        setImgUrl(res.data.data.url)
+        setImgUrl(""+res.data.data.url)
         console.log(imgUrl);
        })
        .catch((err)=>{
