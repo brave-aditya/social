f114e5dbdb3229c8e318aac920e998a3037e34bb
aditya pratap <pratapaditya84@gmail.com>
2023-09-30 08:58:32 +0530

    jjkk

--- src/components/share/share.jsx
+++ src/components/share/share.jsx
@@ -14,7 +14,7 @@ const Share = () => {
 
   const upload = async ()=>{
       const formData = new FormData();
-      formData.append("file", file)
+      formData.append("image", file)
        axios.post('https://api.imgbb.com/1/upload?key=b99e1b7e44deb3985e33be22d597e53f', formData)  
        .then((res)=>{
         console.log(res)
