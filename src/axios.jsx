bdf607f7d2ddff958bdf6034e968ce3e60cf513e
aditya pratap <pratapaditya84@gmail.com>
2023-10-24 18:21:18 +0530

    hj

--- src/axios.jsx
+++ src/axios.jsx
@@ -1,5 +1,9 @@
 import  axios  from "axios";
 
+const token = localStorage.getItem('token');
 export const makeRequest = axios.create({
-    baseURL:"https://social-server-evtu.onrender.com/api"
+    baseURL:"https://social-server-evtu.onrender.com/api",
+    headers: {
+        Authorization: `Bearer ${token}`,
+      }
 })
