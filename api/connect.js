67c661d1377979110b99ade3db2c33dd17b26b46
aditya <pratapaditya84@gmail.com>
2026-03-09 09:10:18 +0530

    Add api folder as a regular directory

---
+++ api/connect.js
@@ -0,0 +1,15 @@
+import mysql from "mysql2"
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+export const db = mysql.createPool({
+   host: process.env.HOST,
+   user: process.env.USER,
+   password: process.env.PASSWORD,
+   database: process.env.DATABASE,
+   port: process.env.DB_PORT || 4000,
+   ssl: {
+     minVersion: 'TLSv1.2',
+     rejectUnauthorized: true
+   }
+});
